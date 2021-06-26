f05247e52a5665de42165ac226cea839a90f46dc
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-06-26 18:34:15 -0300

    Edit application controller. Add first time visit redirection to root.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :first_time_visit, unless: -> { cookies[:first_visit] }
+
+  private
+
+  def first_time_visit
+    cookies.permanent[:first_visit] = 1
+    redirect_to root_path
+  end
 end
