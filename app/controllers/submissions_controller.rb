13cac46caac78d6cc7eec8b7fc358ddf97363d1e
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-06-29 10:06:48 -0300

    Edit submissions controller, add string sanitizer for form input. Add reset session method. Add show logic. Edit create method.

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -5,8 +5,20 @@ class SubmissionsController < ApplicationController
   end
 
   def create
-    @submission = Submission.create(submissions_params)
-    redirect_to "/#{submissions_params[:text]}"
+    sanitized_text = sanitize_text_params(submissions_params[:text])
+    @submission = Submission.create(text: sanitized_text)
+    redirect_to "/#{sanitized_text}"
+  end
+
+  def show
+    @submission = Submission.new
+    @current_submission = Submission.where(text: show_params).last
+    redirect_to root_path if @current_submission.nil?
+  end
+
+  def reset_session
+    session[:created_at] = nil
+    redirect_to root_path
   end
 
   private
@@ -14,4 +26,14 @@ class SubmissionsController < ApplicationController
   def submissions_params
     params.require(:submission).permit(:text)
   end
+
+  def show_params
+    params.require(:path)
+  end
+
+  def sanitize_text_params(string)
+    new_text = string.downcase
+    new_text.gsub(/[^a-z0-9]/, '')
+  end
+
 end
