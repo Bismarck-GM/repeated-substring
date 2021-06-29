11df1f0038479f34bc8d7b784a4c825bb9057ead
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-06-29 10:04:47 -0300

    Edit application controller. Add new action to have all submissions created in session (pulls on every load).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,20 @@
 class ApplicationController < ActionController::Base
   before_action :first_time_visit, unless: -> { cookies[:first_visit] }
+  before_action :set_session_time, unless: -> { session[:created_at] }
+  before_action :set_created_submissions
 
   private
 
   def first_time_visit
     cookies.permanent[:first_visit] = 1
     redirect_to root_path
+  end
+
+  def set_session_time
+    session[:created_at] = Time.current 
+  end
+
+  def set_created_submissions
+    @session_submissions = Submission.where("created_at > ?", session[:created_at])
   end
 end
